7db3af137744edadbbe2073cee15c8c013a0fc0f
bzw <workbzw@outlook.com>
2024-10-06 01:39:42 +0800

    [add] summon [add] createNewHero

--- packages/hardhat/contracts/HeroNFT.sol
+++ packages/hardhat/contracts/HeroNFT.sol
@@ -2,24 +2,31 @@
 pragma solidity ^0.8.0;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract HeroNFT is ERC721 {
-    uint256 private _tokenIdCounter;
+contract HeroNFT is ERC721, Ownable {
+    uint256 private _tokenIdCounter = 0;
+    string private _baseURI = "";
 
-    constructor(string memory name, string memory sym) ERC721(name, sym){
-
+    constructor(address initialOwner, string memory name, string memory sym, string memory _base) ERC721(name, sym) Ownable(initialOwner){
+        _baseURI = _base;
     }
 
-    function mint(address to) public isOwner{
+    function mint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter;
         _safeMint(to, tokenId);
         _tokenIdCounter += 1;
     }
 
-    function burn() public isOwner {
-        
+    function burn(uint256 _tokenId) public onlyOwner {
+        _burn(_tokenId);
     }
 
+    function setBaseURI(string _base) external onlyOwner {
+        _baseURI = _base;
+    }
 
-
+    function _baseURI() internal view virtual returns (string memory) {
+        return _baseURI;
+    }
 }
--- packages/hardhat/contracts/HeroNFTFactory.sol
+++ packages/hardhat/contracts/HeroNFTFactory.sol
@@ -3,43 +3,72 @@ pragma solidity ^0.8.0;
 
 import "./HeroNFT.sol";
 
-contract HeroNFTFactory {
-    address public immutable owner;
+contract HeroNFTFactory is Ownable {
+
     uint256 public price = 10000000000000000;
 
+    mapping(string => address) private _allHeroesMap;
+    HeroNFT[] public allHeroesArr;
+
     event FundsWithdrawn(address indexed owner, uint256 amount);
-    constructor(address _owner){
-        owner = _owner;
-    }
 
-    modifier isOwner() {
-        // msg.sender: predefined variable that represents address of the account that called the current function
-        require(msg.sender == owner, "Not the Owner");
-        _;
-    }
+    constructor(address _initialOwner) Ownable(_initialOwner){}
 
-    function withdraw() public isOwner {
-        uint256 b = address(this).balance;
+    function withdraw() public onlyOwner {
         require(address(this).balance > 0, "No balance!!!");
+        uint256 b = address(this).balance;
         (bool success,) = owner.call{value: address(this).balance}("");
         require(success, "Failed to withdraw!!!");
         emit FundsWithdrawn(owner, b);
     }
 
     receive() external payable {}
-
-    function addNewHero() public isOwner {
-
+    /*
+    * 新建Hero
+    */
+    function createNewHero(string name, string sym) public onlyOwner {
+        HeroNFT hero = new HeroNFT(address(this));
+        _allHeroesMap[name] = address(hero);
+        allHeroesArr.push(hero);
     }
-
+    /*
+    * 抽卡
+    */
     function summon() external payable {
         require(msg.value > price, "Error amount!!!");
-
+        uint256 num = getRandomOnChain() % allHeroesArr.length;
+        allHeroesArr[num].mint(_msgSender());
     }
 
-    function setPrice(uint256 _price) public isOwner {
+    /*
+    * random number for summon
+    */
+    function getRandomOnChain() public view returns (uint256){
+        bytes32 randomBytes = keccak256(abi.encodePacked(block.number, msg.sender, blockhash(block.timestamp - 1)));
+        return uint256(randomBytes);
+    }
+    /*
+    * set price
+    */
+    function setPrice(uint256 _price) public onlyOwner {
         price = _price;
     }
+    /*
+    * mint
+    */
+    function callHeroMint(address _contract, address _to) private {
+        HeroNFT(_contract).mint(_to);
+    }
+    /*
+    * burn
+    */
+    function callHeroBurn(address _contract, uint256 _targetId) private {
+        HeroNFT(_contract).burn(_targetId);
+    }
+    /*
+    * set base uri
+    */
+    function callHeroSetBaseURI(address _contract, string _base) external {
 
-
+    }
 }
